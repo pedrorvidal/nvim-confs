1e3117199faa39b8f52b94481223cdb868a38e62
Pedro Vidal <pedrorvidal@gmail.com>
2025-01-06 18:04:53 -0300

    colorschemes and tscomments

---
+++ lua/plugins/dracula-theme.lua
@@ -0,0 +1,11 @@
+return {
+  "dracula/vim", -- Plugin repository
+  name = "dracula", -- Optional alias for easier reference
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
