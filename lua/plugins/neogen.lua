031c7c21a4e225bfd1891b59ac2cb8dd40dfb118
Pedro Vidal <pedrorvidal@gmail.com>
2025-03-03 22:42:38 -0300

    Update neogen for complete php doc block

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -8,9 +8,25 @@ return {
         languages = {
           php = {
             template = {
-              annotation_convention = "phpdoc", -- Use PHPDoc style
+              annotation_convention = "phpdoc",
+              -- Add PSR-specific configurations
+              use_package_namespace = true, -- Include namespace in doc blocks
+              param_type_first = true, -- Format as @param type $name
+              include_param_description = true,
+              include_return_type = true,
+              include_func_description = true,
+              include_throw_description = true,
+              include_class_description = true,
             },
           },
+        },
+        -- Remove snippet engine specification
+        enable_placeholders = true, -- Add placeholders for descriptions
+        placeholders_text = {
+          description = "Description",
+          param_description = "Parameter description",
+          return_description = "Return value description",
+          throw_description = "Exception description",
         },
       })
     end,
@@ -18,9 +34,16 @@ return {
       {
         "<leader>pd",
         function()
-          require("neogen").generate()
+          require("neogen").generate({ type = "func" })
         end,
-        desc = "Generate PHPDoc",
+        desc = "Generate Function PHPDoc",
+      },
+      {
+        "<leader>pc",
+        function()
+          require("neogen").generate({ type = "class" })
+        end,
+        desc = "Generate Class PHPDoc",
       },
     },
   },
